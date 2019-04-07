6638b4f7c1e4e6eb738ccf843b887aaf9ab2d9d8
akharrou <idev.aymen@hotmail.com>
2019-04-07 15:18:38 -0700

    Latest Update

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
+// Application Dependencies
+// ===============================================================================
+
+/* Import Application Dependencies */
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express');
+const session = require('express-session');
 
+/* Create/Import Routes */
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
+
+// Initialize Application Server
+// ===============================================================================
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// Add MiddleWare
+// ===============================================================================
+
+// Set Path to Static HTML Pages
+app.use(express.static(path.join(__dirname, 'public/html/')));
+
+// Request Body Parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Use 'express-session' to Automatically Create Cookies
+var sess_options = {
+	secret: 'whippersnapper' + Math.floor((Math.random() * 1000000000000) + 1),
+	cookie: { maxAge: 60000 }
+};
+app.use(session(sess_options));
+
+// Set Created Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Export
+// ===============================================================================
 
 module.exports = app;
