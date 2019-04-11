61e1615ba1bf00c12d7bb4d26e17ab0c15fe62ea
Joseph Kaplin <kaplinjoseph@gmail.com>
2019-04-11 06:40:08 -0700

    Add distances to map

--- view/src/components/map.js
+++ view/src/components/map.js
@@ -5,18 +5,87 @@ import "./map.css";
 import Geocode from "react-geocode";
 
 	Geocode.setApiKey("AIzaSyBqKkPlvSSEelPGg4IPqL_2TWyEdYDQeL0");
+	
 
-	Geocode.fromAddress("Eiffel Tower").then(
-		response => {
-			const { lat, lng } = response.results[0].geometry.location;
-			console.log(lat, lng);
-		},
-		error => {
-			console.error(error);
+	// distance formula
+	function degreesToRadians(degrees) {
+		return degrees * Math.PI / 180;
+	}
+
+	function distance(lat1, lon1, lat2, lon2) {
+		var earthRadiusKm = 6371;
+
+		var dLat = degreesToRadians(lat2-lat1);
+		var dLon = degreesToRadians(lon2-lon1);
+
+		lat1 = degreesToRadians(lat1);
+		lat2 = degreesToRadians(lat2);
+
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+						Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		const km_to_miles = 0.62137;
+		return km_to_miles * (earthRadiusKm * c);
+	}
+
+	var restraunts = 
+	[
+	{
+		name: "Cantina",
+		address: '42 Silicon Valley dorms',
+		category: "meh"
+	},
+	{
+		name: "Place to take a selfie",
+		address: 'Golden Gate Bridge',
+		category: "meh"
+	},
+	{
+		name: "Where 42 students work",
+		address: 'Tesla, Fremont',
+		category: "meh"
+	},
+	{
+		name: "Coding House",
+		address: '42 Silicon Valley',
+		category: "meh"
+	}
+	];
+
+	function compare(a, b)
+	{
+		if (a > b)
+		{
+			return (1);
 		}
-	);
+		else if (b > a)
+		{
+			return (-1);
+		}
+		else
+		{
+			return 0;
+		}
+	};
 
-// map function to sort array of restraunt objects, to add to each of them coordinates and distance from user... then to sort and return new array.
+	function sortedArray(restraunts, current_lat, current_lng) {
+		var new_array = restraunts.map(function(restraunt) {
+			Geocode.fromAddress(restraunt.address).then(
+				response => {
+					const { lat, lng } = response.results[0].geometry.location;
+					console.log('coordinates: ' + lat, lng);
+					const dist = distance(current_lat, current_lng, lat, lng);
+					console.log('distance: ' + dist);
+					Object.assign(restraunt, {dist});
+					return restraunt;
+				},
+				error => {
+					console.error(error);
+				}
+			); 
+		});
+		return (new_array.sort(compare));
+	};	
 
 export class MapContainer extends React.Component {
 //	constructor() {
@@ -61,6 +130,7 @@ export class MapContainer extends React.Component {
 		url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
 	};
 
+
 	render() {
 		return (
 			<div className="containerMap">
@@ -69,6 +139,7 @@ export class MapContainer extends React.Component {
 					onClick={this.onMapClicked}
 					zoom={13}
 					initialCenter={{ lat: this.props.lat, lng: this.props.lng}}>
+					<Marker onClick={sortedArray(restraunts, this.props.lat, this.props.lng)}/>
 					<Marker onClick={this.onMarkerClick}
 						title={'The marker`s title will appear as a tooltip.'}
 						name={'SOMA'}
