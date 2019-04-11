982945d349fb47ef4adedde630ab0dd3163756b9
Joseph Kaplin <jkaplin@e1z3r3p1.42.us.org>
2019-04-11 15:20:27 -0700

    P

--- view/src/App.js
+++ view/src/App.js
@@ -4,6 +4,8 @@ import Login from './components/login.js';
 import LandingPage from './components/main.js';
 import Admin from './components/admin.js'
 import { BrowserRouter, Route } from 'react-router-dom';
+import Geocode from "react-geocode";
+Geocode.setApiKey("AIzaSyBqKkPlvSSEelPGg4IPqL_2TWyEdYDQeL0");
 
 class App extends Component {
 
@@ -26,6 +28,7 @@ class App extends Component {
 		this.get_coords = this.get_coords.bind(this);
 		this.set_category = this.set_category.bind(this);
 		this.get_restaurants = this.get_restaurants.bind(this);
+		this.get_sortedArray = this.get_sortedArray.bind(this);
   }
 
 	get_restaurants = () => {
@@ -66,6 +69,50 @@ class App extends Component {
 		}
 	}
 	
+	get_sortedArray = ()  => {
+		const restaurants = this.state.restaurants.filtered;
+		for(let i = 0; i < restaurants.length; i++)
+		{
+			Geocode.fromAddress(restaurants[i].Address_complete).then(
+				response => {
+					const current_lat = this.state.coords.lat;
+					const current_lng = this.state.coords.lng;
+					const { lat, lng } = response.results[0].geometry.location;
+					restaurants[i]['lat'] = lat;
+					restaurants[i]['lng'] = lng;
+					var dist = this.distance(current_lat, current_lng, lat, lng);
+					restaurants[i]['distance'] = dist;
+				},
+				error => {
+					console.error(error);
+				}
+			)
+		}
+		setTimeout(() => {
+			restaurants.sort(function(a, b) {
+				if (!a.distance || a.distance > b.distance)
+				{
+					return (1);
+				}
+				else if (!b.distance || b.distance > a.distance)
+				{
+					return (-1);
+				}
+				else
+				{
+					return 0;
+				}
+			});
+		}, 1);
+		this.setState({
+			restaurants: {
+				restaurants: restaurants,
+				filtered: restaurants,
+				loaded: true
+			}
+		});
+
+	};
 
 	set_category = (e) =>
 	{
@@ -89,10 +136,32 @@ class App extends Component {
 			});
 		setTimeout(() => console.log(this.state.restaurants.filtered), 100);
 	}
+	degreesToRadians = (degrees) => {
+		return degrees * Math.PI / 180;
+	}
+
+	distance = (lat1, lon1, lat2, lon2) => {
+		var earthRadiusKm = 6371;
+
+		var dLat = this.degreesToRadians(lat2-lat1);
+		var dLon = this.degreesToRadians(lon2-lon1);
+
+		lat1 = this.degreesToRadians(lat1);
+		lat2 = this.degreesToRadians(lat2);
+
+		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+						Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		const km_to_miles = 0.62137;
+		return km_to_miles * (earthRadiusKm * c);
+	}
 
 	componentDidMount() {
 		this.get_coords();
 		this.get_restaurants();
+		setTimeout (() => {
+			this.get_sortedArray();
+		}, 1000);
 	}
 
   render() {
