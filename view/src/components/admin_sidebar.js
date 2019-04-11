79de414464df31ed1bd6b764c99bd2ec3b1707d1
Kai Chen <kchen2@e1z3r3p3.42.us.org>
2019-04-11 00:25:19 -0700

    admin visual update

--- view/src/components/admin_sidebar.js
+++ view/src/components/admin_sidebar.js
@@ -5,6 +5,11 @@ import './sidebar.css';
 const SideBar = (props) => {
         return (
             <div className="wrapper">
+            <p className="homeBtn">
+					<a href={"./home"}><img src={require('../HomeIcon.png')}/></a>
+                    <br></br>
+					<a id="txt" href={"./home"}>Home</a>
+			    </p>
             <aside className="main_sidebar">
                 <ul>
                     <li onClick={() => props.change_page('edit')}>Edit information</li>
